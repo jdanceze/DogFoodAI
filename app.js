5c95a9d9160d42470f2005ad69e9ebeadeb30d2f
Pattarakrit Rattanukul <JD@PATTARAKRITQHPFM.local>
2022-10-20 17:42:11 +0800

    db

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 app.use(express.static('public'))
+
+const { MongoClient, ObjectId } = require("mongodb");
+const path = require("path");
+const { get } = require('http');
+const uri =
+  process.env.URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = client.db("userDB");
+const collection = db.collection("user");
+const resultCollection = db.collection("testResult");
+client.connect((err) => {
+  if (err) console.error(err);
+  else console.log("Connected successfully to server");
+});
+
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html');
 });
